7b7b5c87103b36283fb5d9c30afa9d611866de39
David Cris <davidcristhian576@gmail.com>
2026-05-29 15:28:38 -0300

    Criaçao do arquivo script.js, e alteraçao no arquivo index

---
+++ Assets/script/script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+const display = document.querySelector('display');
+
+function Calcular (evento){
+    const peso = parseFloat(document.getElementById('peso_inp').value);
+    const altura = parseFloat(document.getElementById('altura_inp').value);
+
+    if (!peso || !altura) {
+        display.innerHTML = '<p>Por favor, insira valores válidos.</p>';
+        return;
+    }
+
+    const imc = peso / (altura * altura);
+    let resultado = '';
+
+    if (imc < 18.5) {
+        resultado = 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        resultado = 'Peso normal';
+    } else if (imc >= 25 && imc <= 29.9) {
+        resultado = 'Sobrepeso';
+    } else if (imc >= 30 && imc <= 34.9) {
+        resultado = 'Obesidade grau 1';
+    } else if (imc >= 35 && imc <= 39.9) {
+        resultado = 'Obesidade grau 2';
+    } else {
+        resultado = 'Obesidade grau 3';
+    }
+
+    display.innerHTML = `<div><p>Seu IMC é ${imc.toFixed(2)}: ${resultado}</p></div>`;
+}
+
+form.addEventListener('submit', Calcular);
